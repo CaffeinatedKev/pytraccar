5ea2e30907f635110f8f703a3ec56071865bbe67
Caffeinatedkev <larsen.kevin96@gmail.com>
2021-01-14 11:16:38 +0100

    test

---
+++ run.py
@@ -0,0 +1,29 @@
+from pytraccar.api import TraccarAPI
+
+traccar = TraccarAPI('https://gps01.sitesense.no')
+
+
+login = traccar.login_with_token('0fiHy1qGJuyNPECfY4TeRuPeG1on4U2A')
+device = traccar.get_devices()
+print(device)
+car_values = []
+cars = []
+for car in device:
+    if car['name'] == 'SiteSense CHR':
+        car_values.append(car['id'])
+        car_values.append(car['name'])
+        car_values.append(car['status'])
+        car_values.append(car['lastUpdate'])
+print(car_values)
+
+for car in device:
+    car_id = car['id']
+    car_name = car['name']
+    car_status = car['status']
+    car_last_update = car['lastUpdate']
+    print(car_id)
+    print(car_name)
+    print(car_status)
+    print(car_last_update)
+
+
